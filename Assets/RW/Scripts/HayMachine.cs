b4ca18428376bcd16af3df8e0df27a549617f250
Mario229997 <mario.torres01@estudiant.upf.edu>
2023-05-05 23:07:28 +0200

    update p2

--- Assets/RW/Scripts/HayMachine.cs
+++ Assets/RW/Scripts/HayMachine.cs
@@ -7,6 +7,11 @@ public class HayMachine : MonoBehaviour
     public float movementSpeed;
     public float horizontalBoundary = 22;
 
+    public GameObject hayBalePrefab; //Reference to the Hay Bale prefab
+    public Transform haySpawnpoint; //The point from which the hay will to be shot
+    public float shootInterval; //The smallest amount of time between shots
+    private float shootTimer; //A timer that to keep track whether the machine can shoot or not
+
     private void UpdateMovement()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal"); // 1
@@ -23,6 +28,22 @@ public class HayMachine : MonoBehaviour
         Debug.Log(transform.position.x);
     }
 
+    private void UpdateShooting()
+    {
+        shootTimer -= Time.deltaTime;
+
+        if (shootTimer <= 0 && Input.GetKey(KeyCode.Space))
+        {
+            shootTimer = shootInterval;
+            ShootHay();
+        }
+    }
+
+    private void ShootHay()
+    {
+        Instantiate(hayBalePrefab, haySpawnpoint.position, Quaternion.identity);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,5 +54,6 @@ public class HayMachine : MonoBehaviour
     void Update()
     {
          UpdateMovement();
+         UpdateShooting();
     }
 }
